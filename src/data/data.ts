2e607eaa22403effb286e1ed53ad3b563220021d
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-11-25 19:11:49 +0500

    v2.1

--- src/data/data.ts
+++ src/data/data.ts
@@ -85,7 +85,7 @@ export const navigation = [
 	},
 	{
 		label: "Забронировать",
-		href: "#",
+		href: "https://wa.me/77001574697?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9F%D0%B8%D1%88%D1%83%20%D0%BD%D0%B0%D1%81%D1%87%D0%B5%D1%82%20%D0%BD%D0%BE%D0%BC%D0%B5%D1%80%D0%B0%20%D0%B2%20%D0%B3%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B8%D1%86%D0%B5.%20",
 		style: "",
 		icon: "mdi:whatsapp",
 	},
