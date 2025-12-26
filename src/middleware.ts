6ffdedf19f6a4859f851a43cfad693d5165e4e86
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-12-26 17:44:53 +0500

    2

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+
+// import { defineMiddleware } from "astro/middleware"
+
+// export const onRequest = defineMiddleware((ctx, next) => {
+// 	const response = next()
+
+// 	response.headers.set(
+// 		"Cache-Control",
+// 		"public, s-maxage=604800, stale-while-revalidate=86400",
+// 	)
+
+// 	return response
+// })
