b10bdd91aa2a10a7f6b8329e612263386867c2af
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-12-26 22:22:16 +0500

    2

--- src/data/data.ts
+++ src/data/data.ts
@@ -47,12 +47,12 @@ export const socials = [
 	// 	href: "#",
 	// 	social: "tg",
 	// },
-	// {
-	// 	aria_label: "Instagram",
-	// 	title: "@man9",
-	// 	href: "#",
-	// 	social: "inst",
-	// },
+	{
+		aria_label: "Instagram",
+		title: "@hotel_bekarys_astana",
+		href: "https://www.instagram.com/hotel_bekarys_astana/",
+		social: "inst",
+	},
 	// {
 	// 	aria_label: "GitHub",
 	// 	title: "@Kamil12",
@@ -98,20 +98,20 @@ export const rooms_info = {
 export const navigation = [
 	{
 		label: "Отзывы",
-		href: "#yandex",
+		href: "https://www.instagram.com/stories/highlights/17865925371537116/",
 		// target: "_blank",
 		icon: "mdi:account-multiple",
 		style: "",
 	},
+	// {
+	// 	label: "Карта",
+	// 	href: "#yandex",
+	// 	// target: "_blank",
+	// 	icon: "mdi:google-maps",
+	// 	style: "",
+	// },
 	{
-		label: "Карта",
-		href: "#yandex",
-		// target: "_blank",
-		icon: "mdi:google-maps",
-		style: "",
-	},
-	{
-		label: "Забронировать",
+		label: "Написать нам",
 		href: "https://wa.me/77001574697?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9F%D0%B8%D1%88%D1%83%20%D0%BD%D0%B0%D1%81%D1%87%D0%B5%D1%82%20%D0%BD%D0%BE%D0%BC%D0%B5%D1%80%D0%B0%20%D0%B2%20%D0%B3%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B8%D1%86%D0%B5.%20",
 		style: "",
 		icon: "mdi:whatsapp",
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,31 @@
+import { defineMiddleware } from "astro/middleware"
 
-// import { defineMiddleware } from "astro/middleware"
+export const onRequest = defineMiddleware(async (ctx, next) => {
+	const response = await next()
+	const path = ctx.url.pathname
 
-// export const onRequest = defineMiddleware((ctx, next) => {
-// 	const response = next()
+	// ❌ НЕ кэшируем скрипты и стили
+	if (/\.(js|mjs|ts|css)$/i.test(path)) {
+		response.headers.set("Cache-Control", "no-store")
+		return response
+	}
 
-// 	response.headers.set(
-// 		"Cache-Control",
-// 		"public, s-maxage=604800, stale-while-revalidate=86400",
-// 	)
+	// ✅ Кэш изображений (7 дней + immutable)
+	if (/\.(jpg|jpeg|png|webp|avif|gif|svg)$/i.test(path)) {
+		response.headers.set("Cache-Control", "public, max-age=604800, immutable")
+		return response
+	}
 
-// 	return response
-// })
+	// ✅ Кэш HTML-страниц (7 дней)
+	if (
+		!path.includes(".") || // обычные страницы
+		path.endsWith(".html")
+	) {
+		response.headers.set(
+			"Cache-Control",
+			"public, s-maxage=604800, stale-while-revalidate=86400",
+		)
+	}
+
+	return response
+})
